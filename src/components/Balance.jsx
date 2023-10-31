bd765011d9d13be108ca7416db29065fbeb9e9ed
Nicoogf <nicolasgfalabella@gmail.com>
2023-10-31 19:39:26 -0300

    Balance

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -3,11 +3,20 @@ import { useGlobalState } from "../context/GlobalState" ;
 
 const Balance = () => {
 
-  const data = useGlobalState() ;
+  const { transactions }  = useGlobalState() ;
+
+  const amounts = transactions.map( transactions => transactions.amount)
+
+  const total = amounts.reduce((acc, item) => (acc += item) , 0)
+
+ 
   
   return (
     <div>
+      
       <h1>Balance</h1>
+
+      <h3> { total } </h3>
         
     </div>
   )
